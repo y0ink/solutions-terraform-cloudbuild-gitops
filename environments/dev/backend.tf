3a7fcdc4a491f2a0a9cd72ec5168a9df8bbd748d
y0ink <felix.hilmer@txture.io>
2024-10-16 12:20:45 +0200

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "academic-moon-438709-k5-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="academic-moon-438709-k5"
